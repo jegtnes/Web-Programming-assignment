c269d3c2f66531fe9a43dc31f8825cdba3b2332b
Alexander Jegtnes <saccosekk@gmail.com>
2012-03-21 13:28:47 +0000

    Made cart somewhat work

--- modules/cart.inc.php
+++ modules/cart.inc.php
@@ -1,29 +1,50 @@
+<h2>Your cart</h2>
+
 <?php
 //Tutorial http://v3.thewatchmakerproject.com/journal/276/building-a-simple-php-shopping-cart followed when building this cart
 include_once 'modules/dbConnect.inc';
-function displayCart() {
+
+function displayCart($extended = false) {
 	
 	if (isset($_SESSION['cart'])) {
 		$ret = "";
-		print_r($_SESSION['cart']);
+		echo "<table>";
+		
 		foreach($_SESSION['cart'] as $prod) {
 			$id = $prod['id'];
-			
+			$q = $prod['q'];
 			//This statement combines the book and film tables with the main product tables
 			//To avoid getting invalid data, the "AND product.prod_type_id = " in the outer join 
 			//will not assign the properties to the array if they're not appropriate
 			$sql = 
-			"SELECT *
+			"SELECT product.name, product.price
 			FROM product
-			LEFT OUTER JOIN prod_film ON product.product_id = prod_film.product_id AND product.prod_type_id = 1
-			LEFT OUTER JOIN prod_book ON product.product_id = prod_book.product_id AND product.prod_type_id = 0
 			WHERE product.product_id = $id
 			";
 			
 			$result = mysql_query($sql) or die(mysql_error());
-			print_r($row = mysql_fetch_array($result, MYSQL_ASSOC));
+			$row = mysql_fetch_array($result, MYSQL_ASSOC);
+			echo "<tr>";
+			
+			//The amounts
+			echo "<td>" . $q . "x</td>";
+			
+			//the product
+			echo "<td><a href=\"index.php?p=product&id=$id\">" . $row['name'] . "</a></td>";
+			
+			//the price
+			echo "<td>" . ($q * $row['price']) . "</td>";
+			
+			//if we're displaying a simple cart or all the cart info
+			if ($extended == true) {
+				
+				//removal of products
+				echo "<td><a href=\"index.php?p=cart&remove&id=$id\">" . "Remove" . "</a></td>";
+			}
+			
 			
 		}
+		echo "</table>";
 		//return @$ret;
 	}
 
@@ -34,9 +55,42 @@ function displayCart() {
 
 //unset($_SESSION['cart']);
 
-//if you came to the cart wanting to add something
-if (isset($_POST['id'])) {
+//if removing product
+if (isset($_GET['remove']) && isset($_GET['id'])) {
 	
+	if (isset($_SESSION['cart'])) {
+		
+		//prepares a new cart to replace with the older one
+		$newcart;
+		
+		foreach($_SESSION['cart'] as $cart) {
+			
+			//If the product isn't what's being removed, add it to the new cart
+			if($cart['id'] != $_GET['id']) {
+				$newcart[] = $cart;
+			}
+		}
+		
+		//switches carts
+		if(!empty($newcart)) {
+			unset($_SESSION['cart']);
+			$_SESSION['cart'] = $newcart;
+		}
+		
+		else {
+			unset($_SESSION['cart']);
+		}
+		
+		
+	}
+	
+}
+
+//if you came to the cart wanting to add something
+else if (isset($_POST['id'])) {
+	
+	
+	//TODO: If adding product multiple times, modify quantity instead of adding duplicates
 	$id = $_POST['id'];
 	
 	//if we know the quantity
@@ -53,29 +107,19 @@ if (isset($_POST['id'])) {
 	if (!isset($_SESSION['cart'])) {
 		$_SESSION['cart'] = array();
 		$_SESSION['cart'][] = array('id' => $id,'q' => $q);
-		print_r($_SESSION['cart']);
 	}
 
-	//else initialise cart
+	//else add to cart
 	else {
 		$_SESSION['cart'][] = array('id' => $id,'q' => $q);
-		print_r($_SESSION['cart']);
 	}
-	
+	echo (displayCart(true));
 }
 
 //if you didn't want to add something, just display the cart
 else {
-	echo "<pre>";
-	echo (displayCart());
-	echo "</pre>";
+	echo (displayCart(true));
 }
-
-
-
-
-
-
 
 ?>
 <pre>
